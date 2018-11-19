d3d54ce0a1689e51cec725fd09a546e4bc9b52b0
Eleanorigby <jennsungdevelops@gmail.com>
2018-11-19 01:43:12 -0700

    add controllers and model

---
+++ app/controllers/v1/coins_controller.rb
@@ -0,0 +1,2 @@
+class V1::CoinsController < ApplicationController
+end
---
+++ test/controllers/v1/coins_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class V1::CoinsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
